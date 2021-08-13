d8725470a9f511667f97f8a8c895ac935f602a01
Kostya Dzyuba <constanthinium@gmail.com>
2021-08-13 03:17:05 +0300

    Add help menu item

--- app/src/main/java/ru/kostyadzyuba/exam/MainActivity.kt
+++ app/src/main/java/ru/kostyadzyuba/exam/MainActivity.kt
@@ -1,11 +1,33 @@
 package ru.kostyadzyuba.exam
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.help -> {
+                AlertDialog.Builder(this)
+                    .setTitle(item.title)
+                    .setMessage("Tap a test to take it, hold to edit, and swipe to delete.")
+                    .setPositiveButton("OK", null)
+                    .show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
