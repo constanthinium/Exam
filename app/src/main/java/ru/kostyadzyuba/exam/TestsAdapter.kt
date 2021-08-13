77ce0d2bf1971d46ba02ef14cc58db1ef4ed8699
Kostya Dzyuba <constanthinium@gmail.com>
2021-08-13 03:56:52 +0300

    Add list adapter

---
+++ app/src/main/java/ru/kostyadzyuba/exam/TestsAdapter.kt
@@ -0,0 +1,31 @@
+package ru.kostyadzyuba.exam
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TestsAdapter(private val testList: Array<String>) :
+    RecyclerView.Adapter<TestsAdapter.TestViewHolder>() {
+    class TestViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(test: String) {
+            (itemView as TextView).text = test
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TestViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(android.R.layout.simple_selectable_list_item, parent, false)
+        return TestViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TestViewHolder, position: Int) {
+        holder.bind(testList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return testList.size
+    }
+}
